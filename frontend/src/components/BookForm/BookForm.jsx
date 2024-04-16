efc418ab6eaf74089acf7626585f8934a4ac4512
tensor27 <sparksgma@gmail.com>
2024-04-16 19:13:09 +0300

    Added BookForm, BookList and Filter components

---
+++ frontend/src/components/BookForm/BookForm.jsx
@@ -0,0 +1,49 @@
+import Styles from './BookForm.module.css'
+
+import { useState } from 'react'
+
+const BookForm = () => {
+	const [title, setTitle] = useState('')
+	const [author, setAuthor] = useState('')
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+
+		if (title && author){
+			// dispatch action
+			console.log(title, author);
+			setTitle('')
+			setAuthor('')
+		}
+	}
+
+	
+	return (
+		<div className={`app-block ${Styles['book-form']}`}>
+			<h2>Add a New Book</h2>
+			<form onSubmit={handleSubmit}>
+				<div>
+					<label htmlFor='title'>Title: </label>
+					<input
+						type='text'
+						id='title'
+						value={title}
+						onChange={e => setTitle(e.target.value)}
+					/>
+				</div>
+				<div>
+					<label htmlFor='author'>Author: </label>
+					<input
+						type='text'
+						id='author'
+						value={author}
+						onChange={e => setAuthor(e.target.value)}
+					/>
+				</div>
+				<button type='submit'>Add book</button>
+			</form>
+		</div>
+	)
+}
+
+export default BookForm
---
+++ frontend/src/components/Filter/Filter.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Filter() {
+	return <div className='app-block filter'>Filter</div>
+}
+
+export default Filter
