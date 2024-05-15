6dc54112810c4cc3a78a28406e38bb31790e378c
tensor27 <sparksgma@gmail.com>
2024-05-15 14:56:16 +0300

    Created errorSlice

---
+++ frontend/src/redux/slices/errorSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+	error: null,
+	date: null,
+}
+
+const errorSlice = createSlice({
+	name: 'error',
+	initialState,
+	reducers: {
+		setError: (state, action) => {
+			return action.payload
+		},
+		clearError: () => {
+			return initialState
+		},
+	},
+})
+
+export const { setError, clearError } = errorSlice.actions
+
+export const selectErrorMessage = state => state.error
+
+export default errorSlice.reducer
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import booksReducer from './slices/bookSlice'
+import errorReducer from './slices/errorSlice'
 import filterReducer from './slices/filterSlice'
 
 const store = configureStore({
 	reducer: {
 		books: booksReducer,
 		filter: filterReducer,
+		error: errorReducer,
 	},
 })
 
